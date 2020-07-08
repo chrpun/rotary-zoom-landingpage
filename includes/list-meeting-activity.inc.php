984bb2dc93a6cddc9f4b04c03025ff7b5cf9a85a
Christian Punke <59334598+chrpun@users.noreply.github.com>
2020-07-08 11:52:43 +0200

    preparation for merge with SSO-branch
    Seiten sind nun keine "kompletten" HTML Seiten mehr, sondern werden von einer Admin-Seite mit SSO-Login eingebunden. Dafür werden die Branches gleich zusammengeführt.

--- includes/list-meeting-activity.inc.php
+++ includes/list-meeting-activity.inc.php
@@ -1,8 +1,4 @@
 <?php
-// Debug-Modus!
-ini_set('display_errors', true);
-ini_set('error_reporting', E_ALL);
-
 /*
 ========================================================
 Rotary Club Zoom Landing Page
@@ -10,24 +6,33 @@ Copyright 2020 Christian Punke, Rotary E-Club of D-1850
 https://github.com/chrpun/rotary-zoom-landingpage
 ========================================================
 
-list-meeting-activity.php
->> Listet die 5 letzten Meetings mit den Teilnehmern auf
-
-noch rudimentär und in Entwicklung!
+list-meeting-activity.inc.php
+>> Bereitet eine Liste für den Aufruf von meta-forward_sso.inc.php vor.
+>> wird von meta-forward_sso.inc.php eingebunden
 
 ========================================================
 */
 
-// Laden der Einstellungen und benötigen Funktionen
-require __DIR__ . '/includes/settings.inc.php';
-
-
-
-
-if (!$registration_enabled) {
-  ###### HINWEIS: FUNKTIONIERT NUR MIT "?register-test=1"
-  #die('Die hinterlegte Meeting-ID ('.$meeting_id.', '.$meeting_name.') hat keine Registrierung aktiviert... Somit gibt es hier nichts zu sehen...');
+// Sicherstellen, dass diese Seite nur von meta-forward_sso.inc.php aufgerufen wird und nicht eigenständig
+if (!isset($show_activity_list) || !$show_activity_list) {
+  header('Location: ../index.php');
+  die();
 }
+
+// Anzahl der Meetings die ausgegeben werden
+// wenn nicht gesetzt - Standard-Wert = 3;
+if (!isset($number_of_meetings)) $number_of_meetings = 3;
+
+// Min. Länge eines Meetings in Minuten, damit es angezeigt wird (um zB Test-Sessions nicht anzuzeigen)
+// wenn nicht gesetzt - Standard-Wert = 30;
+if (!isset($min_meeting_length)) $min_meeting_length = 30;
+
+// Min. Länge die ein Teilnehmer im Meeting war um angezeigt zu werden (sonst werden zB alle Teilnehmer doppelt angezeigt wenn diese im Warteraum waren)
+// wenn nicht gesetzt - Standard-Wert = 2;
+if (!isset($min_participant_length)) $min_participant_length = 2;
+
+
+
 
 $list = list_last_meetings($meeting_id); // Liste mit allen UUIDs
 
@@ -40,18 +45,25 @@ foreach ($list as $v1) {
   
   $uuid = $v1['uuid'];
   
-  $data = meeting_report($uuid);
+  $data2 = meeting_report($uuid);
+  // echo $uuid.'<br>'; // Debug-Only
   
-  $array[$i]['start_time'] = strtotime($data['start_time']);
-  $array[$i]['end_time'] = strtotime($data['end_time']);
-  $array[$i]['duration'] = $data['duration']; // ungenau... nur ganze minuten...
+  $array[$i]['start_time'] = strtotime($data2['start_time']);
+  $array[$i]['end_time'] = strtotime($data2['end_time']);
+  $array[$i]['duration'] = $data2['duration']; // ungenau... nur ganze minuten...
   $array[$i]['duration'] = ($array[$i]['end_time'] - $array[$i]['start_time']) / 60; // besser :)
   
+  // nur Meetings anzeigen, die min 20 Minuten gedauert haben (Rest weren vermutlich Tests etc...)
+  if ($array[$i]['duration'] < $min_meeting_length) continue;
   
-  $data2 = participant_meeting_report($uuid);
+  
+  $data3 = participant_meeting_report($uuid);
   
   $j = 0;
-  foreach ($data2 as $v) {
+  foreach ($data3 as $v) {
+    
+    if (($v['duration'] / 60) < $min_participant_length) continue;
+    
     $array[$i]['participant'][$j]['name'] = $v['name'];
     $array[$i]['participant'][$j]['user_email'] = $v['user_email'];
     $array[$i]['participant'][$j]['join_time'] = strtotime($v['join_time']);
@@ -63,8 +75,8 @@ foreach ($list as $v1) {
   
   $i++;
   
-  // Nach 5 Meetings Schluss machen...
-  if ($i>4) {
+  // Nach x Meetings Schluss machen...
+  if ($i >= $number_of_meetings) {
     break;
   }
 }
@@ -72,43 +84,16 @@ foreach ($list as $v1) {
 
 #var_dump($array); die();
 
-?><!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Zoom Meeting Landing Page of <?php echo $club_name ?>">
-  <meta name="author" content="Christian Punke, Rotary E-Club of D-1850">
-  
-  <!--
-  Rotary Club Landing Page Software
-    Copyright 2020 Christian Punke, Rotary E-Club of D-1850
-    Please see license information on https://github.com/chrpun/rotary-zoom-landingpage
-  -->
-
-  <title><?php echo $club_name ?> // Liste Meeting Aktivität</title>
-
-  <!-- CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/main.css" >
-  
-</head>
-
-<body>
 
 
-<h3>Letzte Meetings und entsprechende Aktivität</h3>
-
-<?php foreach ($array as $line): ?>
-  
-  <h5 class="mt-5">Meeting (Start: <?php echo date("d.F Y H:i", $line['start_time']) ?>)</h5>
+foreach ($array as $line): ?>
+  <h5 class="mt-5"><?php echo date("D, d F Y H:i", $line['start_time']) ?> (Dauer: <?php echo round($line['duration'], 0) ?> Min.)</h5>
   
   <table class="table-sm table-striped table-hover table-bordered">
     <thead>
       <tr>
-        <th>Teilehmer</th><th>Mail</th><th>Teilnahmedauer</th><th>Gesamt-Meetingdauer</th><th>Teilnahmeanteil %</th><th>Präsenzkarte schicken</th>
+        <!-- <th>Teilnehmer</th><th>Mail</th><th>Teilnahmedauer</th><th>Gesamt-Meetingdauer</th><th>Teilnahmeanteil %</th><th>Präsenzkarte schicken</th> -->
+        <th>Teilnehmer</th><th>Teilnahmedauer</th><th>Teilnahmeanteil %</th>
       </tr>
     </thead>
     
@@ -117,22 +102,13 @@ foreach ($list as $v1) {
     <?php foreach ($line['participant'] as $line2): ?>
       <tr>
         <td><?php echo $line2['name'] ?></td>
-        <td><?php echo $line2['user_email'] ?></td>
+        <!-- <td><?php echo $line2['user_email'] ?></td> -->
         <td><?php echo round($line2['duration'], 0) ?> Min.</td>
-        <td><?php echo round($line['duration'], 0) ?> Min.</td>
+        <!-- <td><?php echo round($line['duration'], 0) ?> Min.</td> -->
         <td><?php $prozent = $line2['duration'] / $line['duration']; echo round($prozent*100, 0) ?> % </td>
-        <td><?php echo ($prozent > 0.6) ? '<a href="#" class="btn btn-sm btn-outline-success" role="button">Karte schicken</a>' : 'nein...' ?></td>
+        <!-- <td><?php echo ($prozent > 0.6) ? '<a href="#" class="btn btn-sm btn-outline-success" role="button">Karte schicken</a>' : 'nein...' ?></td> -->
       </tr>
     <?php endforeach ?>
   </tbody></table>
   
 <?php endforeach ?>
-
-<div class="mt-4"><a href="list-participants.php?register-test=1">Alle aktuell registrierten Teilnehmer anzeigen</a></div>
-
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-</body>
-</html>
--- includes/list-participants.inc.php
+++ includes/list-participants.inc.php
@@ -1,8 +1,4 @@
 <?php
-// Debug-Modus!
-ini_set('display_errors', true);
-ini_set('error_reporting', E_ALL);
-
 /*
 ========================================================
 Rotary Club Zoom Landing Page
@@ -10,58 +6,34 @@ Copyright 2020 Christian Punke, Rotary E-Club of D-1850
 https://github.com/chrpun/rotary-zoom-landingpage
 ========================================================
 
-list-participants.php
+list-meeting-activity.inc.php
 >> Listet die aktuell angemeldeten Teilnehmern für die nächste Meeting-Instanz auf
-
-noch rudimentär und in Entwicklung!
+>> wird von meta-forward_sso.inc.php eingebunden
 
 ========================================================
 */
 
-// Laden der Einstellungen und benötigen Funktionen
-require __DIR__ . '/includes/settings.inc.php';
-
-
-if (!$registration_enabled) {
-  ###### HINWEIS: FUNKTIONIERT NUR MIT "?register-test=1"
-  die('Die hinterlegte Meeting-ID ('.$meeting_id.', '.$meeting_name.') hat keine Registrierung aktiviert... Somit gibt es hier nichts zu sehen...');
+// Sicherstellen, dass diese Seite nur von meta-forward_sso.inc.php aufgerufen wird und nicht eigenständig
+if (!isset($show_participant_list) || !$show_participant_list) {
+  header('Location: ../index.php');
+  die();
 }
 
 $list = list_all_participants($meeting_id);
 
-?><!DOCTYPE html>
-<html lang="en">
+?>
+<h5>Teilnehmerliste</h5>
 
-<head>
+<?php if (!$registration_enabled): ?>
+  Die hinterlegte Meeting-ID (<?php echo $meeting_id ?>, <?php echo $meeting_name ?>) hat keine Registrierung aktiviert... Somit gibt es hier nichts zu sehen...
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Zoom Meeting Landing Page of <?php echo $club_name ?>">
-  <meta name="author" content="Christian Punke, Rotary E-Club of D-1850">
-  
-  <!--
-  Rotary Club Landing Page Software
-    Copyright 2020 Christian Punke, Rotary E-Club of D-1850
-    Please see license information on https://github.com/chrpun/rotary-zoom-landingpage
-  -->
-
-  <title><?php echo $club_name ?> // Teilnehmer-Liste</title>
-
-  <!-- CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/main.css" >
-  
-</head>
-
-<body>
-
-
-<h3>Teilnehmerliste</h3>
+<?php else: ?>
 
 <table class="table-sm table-striped table-hover table-bordered">
   <thead>
     <tr>
-      <th>Vorname</th><th>Nachname</th><th>E-Mail</th><th>Status</th><th>Reg.-Zeit</th><th>Join URL</th>
+      <!-- <th>Vorname</th><th>Nachname</th><th>E-Mail</th><th>Status</th><th>Reg.-Zeit</th><th>Join URL</th> -->
+      <th>Vorname</th><th>Nachname</th><th>E-Mail</th><th>Status</th><th>Reg.-Zeit</th>
     </tr>
   </thead>
   
@@ -76,13 +48,4 @@ foreach($list['registrants'] as $value) {
 }
 ?>
 </tbody></table>
-
-
-<div class="mt-4"><a href="list-meeting-activity.php?register-test=1">Alle Teilnehmer der letzten Meetings anzeigen</a></div>
-
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php endif ?>
