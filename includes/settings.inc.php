55a938fe9398b496d5dd8a0496cb6f7ca860590c
Christian Punke <59334598+chrpun@users.noreply.github.com>
2020-12-10 23:03:07 +0100

    delete president name
    obsolet, da Name auch im Impressum gelöscht wurde.

--- includes/settings.inc.php
+++ includes/settings.inc.php
@@ -73,7 +73,6 @@ $dataprotection_filename = 'data-protection-example.pdf';
 $help_filename = 'help-example.pdf';
 
 // Für Impressum:
-$praesident = 'Max Mustermann';
 $praesident_mail = 'max@mustermann.de';
 
 //Social Buttons (wenn nicht vorhanden - leer (also '') lassen, dann wird das Icon nicht angezeigt.)
